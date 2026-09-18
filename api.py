d10f979cc3c21a2bf51b8aa6cfaf9aac9745cee1
jajawnwnn91188 <vagepo9598@duck.com>
2026-09-18 12:33:56 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1079,6 +1079,6 @@ def shopify_checker():
             "cc": request.args.get('cc', '')
         }), 500
 
-# Coded by @orion_store_07
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
